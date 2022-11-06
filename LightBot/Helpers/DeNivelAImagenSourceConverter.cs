b3e4f78254ec7a744873072b8b7575533d47f1a8
Angel Villafan <191g0239@rcarbonifera.tecnm.mx>
2022-11-06 15:47:28 -0600

    hola

---
+++ LightBot/Helpers/DeNivelAImagenSourceConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace LightBot.Helpers
+{
+    internal class DeNivelAImagenSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int nivel = (int)value;
+            string ruta = "";
+            if (nivel == 1)
+            {
+                ruta = "../Assets/Instrucciones1.png";
+            }
+            else if (nivel == 2)
+            {
+                ruta = "../Assets/Instrucciones2.png";
+
+            }
+            return ruta;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
